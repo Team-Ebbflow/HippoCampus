7d0309a7c6794f08580e6b966881858bd7fc2c70
CaesarLiew <lh997072@gmail.com>
2022-06-21 20:48:28 +0800

    login page

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router';
+import React from 'react'
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
-import { Login } from './components/Login';
+import Login from './components/Login';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './custom.css'
 
-export default class App extends Component {
-  static displayName = App.name;
-
-  render () {
+function App() {
     return (
-      <Layout>
-        <Route exact path='/' component={Home} />
-        <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
-        <Route path='/login' component={Login} />
-      </Layout>
-    );
-  }
+        <Router>
+            <div className="App">
+                <div className="content">
+                    <Switch>
+                        <Route path="/">
+                            <Login />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+        );
 }
+
+export default App;
---
+++ ClientApp/src/components/management.js
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HippocampusUON.ClientApp.src.components
+{
+    public class Component
+    {
+    }
+}
