e32bd252bf8bf997970a7bcf9c521b5d437606b9
Qi Shengen <cest.andrew@outlook.com>
2022-06-21 21:33:06 +0800

    Change every pages to use React Hooks

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,29 +1,29 @@
 import React from 'react'
-import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
+import NavMenu from './components/NavMenu';
+import EventPage from './components/EventPage';
+import Home from './components/Home';
 import Login from './components/Login';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
 import './custom.css'
 
 function App() {
-    return (
-        <Router>
-            <div className="App">
-                <div className="content">
-                    <Switch>
-                        <Route path="/">
-                            <Login />
-                        </Route>
-                    </Switch>
-                </div>
-            </div>
-        </Router>
-        );
+  return (
+    <div>
+      <Router>
+        <NavMenu />
+        <div>
+        <Switch>
+          <Route exact={true} path="/home" component={Home} />
+          <Route exact path="/event" component={EventPage} />
+          <Route exact path="/login" component={Login} />
+          <Route component={Home} />
+        </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ ClientApp/src/components/EventPage.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+export default function EventPage() {
+
+  return (
+    <div>
+      <h1>FUCK YOU LIU HAO</h1>
+    </div>
+
+  );
+}
