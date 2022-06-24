436923a95e268e30b8b88f202fef3ec6cca8aa62
Lim Hao Jie <100665922+hjie98@users.noreply.github.com>
2022-06-24 17:19:24 +0800

    club

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -3,7 +3,8 @@ import NavMenu from './components/NavMenu';
 import EventPage from './components/EventPage';
 import Home from './components/Home';
 import Login from './components/Login';
-import Guide from './components/Guide'
+import Guide from './components/Guide';
+import Club from './components/Club';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/event" component={EventPage} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/guide" component={Guide} />
+          <Route exact path="/club" component={Club} />
           <Route component={Home} />
         </Switch>
         </div>
---
+++ ClientApp/src/components/Club.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import './Club.css';
+
+export default function Club() {
+
+
+
+    return (
+        <div>
+        </div>
+
+    );
+}
